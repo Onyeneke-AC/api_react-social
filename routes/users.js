02e884af17df80140dd920cc109bb294bb86e8c0
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-07 12:01:39 +0100

    create a post

--- routes/users.js
+++ routes/users.js
@@ -50,7 +50,9 @@ router.delete("/:id", async (req,res) => {
 
 router.get("/:id", async (req, res) => {
     try {
+        // find the user
         const user = await User.findById(req.params.id);
+        // leave out the properties not needed
         const {password, updatedAt, ...other} = user._doc;
         res.status(200).json(other)
     } catch(err) {
@@ -71,7 +73,9 @@ router.put("/:id/follow", async (req, res) => {
 
             // check if current user  is already following that user
             if(!user.followers.includes(req.body.userId)){
+                //if they're not, add them to the array of followers and save it in the database
                 await user.updateOne({$push: {followers: req.body.userId}});
+                //also add to the array of followings and save to the database
                 await currentUser.updateOne({$push: {followings: req.params.id}});
                 res.status(200).json("User has been followed");
             }else {
@@ -96,9 +100,11 @@ router.put("/:id/unfollow", async (req, res) => {
             //also found the one trying to make request
             const currentUser = await User.findById(req.body.userId);
 
-            // check if current user  is already following that user
+            // check if current user is already following that user
             if(user.followers.includes(req.body.userId)){
+                // if they are, use the pull method to pull them from the array of followers 
                 await user.updateOne({$pull: {followers: req.body.userId}});
+                // if they are, use the pull method to pull them from the array of followers
                 await currentUser.updateOne({$pull: {followings: req.params.id}});
                 res.status(200).json("User has been unfollowed");
             }else {
