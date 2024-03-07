ea1cd4a1013ddf3fd42e78411a36f6e85e8efa25
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-07 08:38:46 +0100

    delete a user implementation

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 // update user
 
 router.put("/:id", async (req,res) => {
-    if(req.body.userId === req.params.id || req.user.isAdmin){
+    if(req.body.userId === req.params.id || req.body.isAdmin){
         if(req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -20,7 +20,7 @@ router.put("/:id", async (req,res) => {
         }
         try {
             const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
-            res.status(200).json("Account has been updated");
+            res.status(200).json("Account has been deleted");
         } catch {
             return res.status(500).json(err)
         }
@@ -31,6 +31,20 @@ router.put("/:id", async (req,res) => {
 })
 
 // delete user
+
+router.delete("/:id", async (req,res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted");
+        } catch {
+            return res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You can delete only your account");
+    }
+
+})
 
 // get a user
 
