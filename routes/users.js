07256b880956c3f37a0f7c47b9aa71b8456353f7
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-07 01:09:27 +0100

    updating needed user info

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,14 @@ router.get("/", (req, res) => {
     res.send("hey it's users route");
 })
 
+// update user
+
+// delete user
+
+// get a user
+
+// follow a user
+
+// unfollow a user
+
 module.exports = router;
